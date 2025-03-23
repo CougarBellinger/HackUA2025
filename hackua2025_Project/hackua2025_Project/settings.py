0601e68e23f745d06675c789d7e4ee64c87c319c
Karan Patel <karanpatel@arizona.edu>
2025-03-22 19:40:22 -0700

    Initial sample page with cards

--- hackua2025_Project/hackua2025_Project/settings.py
+++ hackua2025_Project/hackua2025_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travel_buddy',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hackua2025_Project/hackua2025_Project/urls.py
+++ hackua2025_Project/hackua2025_Project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from travel_buddy import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('travel-question/', views.travel_question, name='travel_question')
 ]
