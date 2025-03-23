d2e324f710f1839b366de93b23aa97df41846615
cougarbellinger <cougarbellinger@arizona.edu>
2025-03-22 17:08:18 -0700

    initialized django project again using venv and imported a template .gitignore

--- hackua2025_Project/hackua2025_Project/wsgi.py
+++ hackua2025_Project/hackua2025_Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for travel_buddy project.
+WSGI config for hackua2025_Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_buddy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackua2025_Project.settings')
 
 application = get_wsgi_application()
