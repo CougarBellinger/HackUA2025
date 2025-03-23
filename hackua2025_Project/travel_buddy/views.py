ededf97a54423b3a6ea67523fbbe9be6d3c5468e
Eden <edengeyer@arizona.edu>
2025-03-22 19:38:43 -0700

    start of mood url, view, html

--- hackua2025_Project/travel_buddy/views.py
+++ hackua2025_Project/travel_buddy/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mood(request):
+    moods = ["Relaxed", "Adventurous", "Inspired", "Pampered", "Connected", "Knowledgeable", "Artistic"]  # Hardcoded moods
+    return render(request, 'mood.html', {'moods': moods})
+
