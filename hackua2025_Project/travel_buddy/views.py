c4c85853edc763b1bdb06f4836021392e66b22c8
Eden <edengeyer@arizona.edu>
2025-03-22 20:22:09 -0700

    got website to open now

--- hackua2025_Project/travel_buddy/views.py
+++ hackua2025_Project/travel_buddy/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def mood(request):
     moods = ["Relaxed", "Adventurous", "Inspired", "Pampered", "Connected", "Knowledgeable", "Artistic"]  # Hardcoded moods
-    return render(request, 'mood.html', {'moods': moods})
+    return render(request, 'travel_buddy/mood.html', {'moods': moods})
 
